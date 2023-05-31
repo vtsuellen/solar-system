bf7415b410115ddea1bde3b9ac5a7a32a838cc4c
vtsuellen <vtsuellen@hotmail.com>
2023-05-31 19:12:43 -0300

    feat: Criar componente Missions e renderizar o componente Title

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import './App.css';
+import Missions from './components/Missions';
 
 function App() {
   return (
     <>
       <Header />
       <SolarSystem />
-      {/* <PlanetCard /> */}
+      <Missions />
     </>
   );
 }
---
+++ src/components/Missions.tsx
@@ -0,0 +1,13 @@
+import Title from './Title';
+
+function Missions() {
+  return (
+    <div data-testid="missions">
+      <Title
+        headline="Missões"
+      />
+    </div>
+  );
+}
+
+export default Missions;
