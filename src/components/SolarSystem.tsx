9ae71d0d52ece41d670680530f878485ce40fba3
vtsuellen <vtsuellen@hotmail.com>
2023-05-31 18:58:02 -0300

    feat: Adicionar lista de planetas ao SolarSystem utilizando o componente PlanetCard

--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
 import './css/Solar.css';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 function SolarSystem() {
   return (
     <div data-testid="solar-system">
       <Title headline="Planetas" />
+
+      <ul>
+        {planets.map((planet) => (
+          <li key={ planet.name }>
+            <PlanetCard planetImage={ planet.image } planetName={ planet.name } />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
