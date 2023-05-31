8be01f1c5ab95b4a8af938039b379e6824e59d46
vtsuellen <vtsuellen@hotmail.com>
2023-05-31 15:36:21 -0300

    fix: Corrigindo erros do lint no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/header/Header';
 import SolarSystem from './components/SolarSystem';
+import './App.css';
 
 function App() {
   return (
